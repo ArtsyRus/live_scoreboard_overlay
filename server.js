33c6b99735c8fdc9c0878f220a49cfada164051c
ArtsyRus <artsyruscom@gmail.com>
2025-06-22 16:39:01 -0400

    server_listen

--- server.js
+++ server.js
@@ -92,6 +92,6 @@ app.post('/set-drivers', (req, res) => {
 });
 
 app.use(express.static('public'));
-server.listen(3000, () => {
+server.listen(3000, '0.0.0.0',() => {
   console.log('Server running at http://localhost:3000');
 });
